a19ec81eb8ed2d98b8834c4829181fa2e44148db
COGOLUENHES REMI p1909391 <remi.cogoluenhes@etu.univ-lyon1.fr>
2022-12-13 17:01:02 +0100

    Add TopRibbon and fix navbar style

--- src/components/atoms/MenuButton/MenuButton.jsx
+++ src/components/atoms/MenuButton/MenuButton.jsx
@@ -6,6 +6,10 @@ export const MenuButton = (props) =>  {
     }
     
     return (
+        <>
             <button className="menuButton" onClick={contentHandler}>{props.children}</button>
+            {/* <Divider light={true} sx={{paddingBottom: "10px", borderRadius: "10px", color:"white", zIndex:1000}}></Divider> */}
+        </>
+
     )
 }
