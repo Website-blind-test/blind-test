c55c7f6be76a5a9fc1ec8ee20d6bafebb9e701de
AlexCathe <al.cathelain@gmail.Com>
2022-12-15 12:19:00 +0100

    Fixs

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,41 +1,24 @@
 export const getTheme = (theme) => {
-    if(theme){
-        return {
-            background:"#62D9FF",
-            primary:"green",
-            label:"#00FFD1",
-            marginTop: '2rem',
-            secondary:"",
-            validate:"",
-            error:""
-        }
-    }
-    return {
-        background:"#62D9FF",
-        primary:"cyan",
-        label:"#00FFD1",
-        marginTop: '5rem',
-        secondary:"",
-        validate:"",
-        error:"",
-        container:{
-            primary:"",
-            secondary:"",
-            validate:"",
-            error:"",
-        },
-        on: {
-            primary:"",
-            secondary:"",
-            validate:"",
-            error:"",
-            container:{
-                primary:"",
+    switch(theme){
+        case 'signup':
+            return {
+                background:"#62D9FF",
+                primary:"green",
+                label:"#00FFD1",
+                marginTop: '2rem',
                 secondary:"",
                 validate:"",
-                error:"",
-            }    
+                error:""
+            }
+        case 'login':
+            return {
+                background:"#62D9FF",
+                primary:"cyan",
+                label:"#00FFD1",
+                marginTop: '5rem',
+                secondary:"",
+                validate:"",
+                error:""
+            }
         }
-        
-    }
 }
