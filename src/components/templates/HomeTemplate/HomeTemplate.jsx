027424eeb53c6d6fa91da5a4f48ddb25aacff9fb
AlexCathe <al.cathelain@gmail.Com>
2023-01-03 15:54:04 +0100

    Adding of game page files

--- src/components/templates/HomeTemplate/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate/HomeTemplate.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { TopRibbon } from "../../organisms/TopRibbon/TopRibbon";
 import {HomeOrga} from "../../organisms/HomeOrga/HomeOrga";
+import { useSelector } from "react-redux";
 
 export const HomeTemplate = () => {
+
+    const {username} = useSelector((state) => state.auth.userInfo)
+
     return(
         <>
-            <TopRibbon>Bienvenue pseudo !</TopRibbon>
+            <TopRibbon>Bienvenue {username} !</TopRibbon>
             <HomeOrga/>
         </>
     )
