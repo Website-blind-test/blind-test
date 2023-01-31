d4cac3247a221bca365b234d23e67ab5a240f582
AlexCathe <al.cathelain@gmail.Com>
2023-01-31 22:14:43 +0100

    Tentative PWA

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import Offline from './components/Offline/Offline';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <>
       <App />
     <Offline></Offline>
-  </>
   </React.StrictMode>
 );
 
